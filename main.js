acb8b75387253a16856382f4de365d3f74cac2a4
Asif-102 <aiqbal517@gmail.com>
2021-09-08 21:38:41 +0600

    fix some bugs

--- main.js
+++ main.js
@@ -1,49 +1,61 @@
-document.getElementById('issueInputForm').addEventListener('submit', submitIssue);
+document
+	.getElementById("issueInputForm")
+	.addEventListener("submit", submitIssue);
 
+  // Submit:
 function submitIssue(e) {
-  const getInputValue = id => document.getElementById(id).value;
-  const description = getInputValue('issueDescription');
-  const severity = getInputValue('issueSeverity');
-  const assignedTo = getInputValue('issueAssignedTo');
-  const id = Math.floor(Math.random()*100000000) + '';
-  const status = 'Open';
+	const getInputValue = (id) => document.getElementById(id).value;
+	const description = getInputValue("issueDescription");
+	const severity = getInputValue("issueSeverity");
+	const assignedTo = getInputValue("issueAssignedTo");
+	const id = Math.floor(Math.random() * 100000000) + "";
+	const status = "Open";
 
-  const issue = { id, description, severity, assignedTo, status };
-  let issues = [];
-  if (localStorage.getItem('issues')){
-    issues = JSON.parse(localStorage.getItem('issues'));
-  }
-  issues.push(issue);
-  localStorage.setItem('issues', JSON.stringify(issues));
+	const issue = { id, description, severity, assignedTo, status };
+	let issues = [];
+	if (localStorage.getItem("issues")) {
+		issues = JSON.parse(localStorage.getItem("issues"));
+	}
+	issues.push(issue);
+	localStorage.setItem("issues", JSON.stringify(issues));
 
-  document.getElementById('issueInputForm').reset();
-  fetchIssues();
-  e.preventDefault();
+	document.getElementById("issueInputForm").reset();
+	fetchIssues();
+	e.preventDefault();
 }
 
-const closeIssue = id => {
-  const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find(issue => issue.id === id);
-  currentIssue.status = 'Closed';
-  localStorage.setItem('issues', JSON.stringify(issues));
-  fetchIssues();
-}
+// closed Function:
+const setStatusClosed = (id) => {
+	const issues = JSON.parse(localStorage.getItem("issues"));
+	const currentIssue = issues.find((issue) => issue.id == id);
+	currentIssue.status = "Closed";
+	localStorage.setItem("issues", JSON.stringify(issues));
+	fetchIssues();
+};
 
-const deleteIssue = id => {
-  const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
-  localStorage.setItem('issues', JSON.stringify(remainingIssues));
-}
+
+// Delete Function:
+const deleteIssue = (id) => {
+	const issues = JSON.parse(localStorage.getItem("issues"));
+	const remainingIssues = issues.filter((issue) => issue.id != id);
+
+	localStorage.setItem("issues", JSON.stringify(remainingIssues));
+	location.reload();
+};
+
+// fetch:
 
 const fetchIssues = () => {
-  const issues = JSON.parse(localStorage.getItem('issues'));
-  const issuesList = document.getElementById('issuesList');
-  issuesList.innerHTML = '';
+	const issues = JSON.parse(localStorage.getItem("issues"));
+	const issuesList = document.getElementById("issuesList");
+	issuesList.innerHTML = "";
 
-  for (var i = 0; i < issues.length; i++) {
-    const {id, description, severity, assignedTo, status} = issues[i];
+	for (var i = 0; i < issues.length; i++) {
+		const { id, description, severity, assignedTo, status } = issues[
+			i
+		];
 
-    issuesList.innerHTML +=   `<div class="well">
+		issuesList.innerHTML += `<div class="well">
                               <h6>Issue ID: ${id} </h6>
                               <p><span class="label label-info"> ${status} </span></p>
                               <h3> ${description} </h3>
@@ -52,5 +64,35 @@ const fetchIssues = () => {
                               <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
-  }
-}
+	}
+	openList();
+};
+
+// OpenList Function:
+let openList = () => {
+	let show = document.getElementById("issuesList").childElementCount;
+	document.getElementById("TrackerCount").innerText = show;
+
+ 
+	let potStatus = document.getElementsByClassName("label label-info");
+
+	if (potStatus.length > 0) {
+		document.getElementById("description").style.display = "block";
+		openCount = 0;
+		closeCount = 0;
+
+		for (let i = 0; i < potStatus.length; i++) {
+			const element = potStatus[i].innerText;
+			if (element == "Open") {
+				openCount = openCount + 1;
+			} else if (element == "Closed") {
+				closeCount = closeCount + 1;
+			}
+		}
+
+		document.getElementById("open").innerText = openCount;
+		document.getElementById("closed").innerText = closeCount;
+	} else {
+		document.getElementById("description").style.display = "none";
+	}
+};
